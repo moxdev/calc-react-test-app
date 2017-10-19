1836b0611183f03e2c3f2ccc4b516c53e8149844
moxdev <moxdev1@gmail.com>
2017-10-19 17:46:01 -0400

    got a key issue

--- my-app/src/components/Display.js
+++ my-app/src/components/Display.js
@@ -4,19 +4,41 @@ import PropTypes from 'prop-types';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Dialog from 'material-ui/Dialog';
 import FlatButton from 'material-ui/FlatButton';
 
 class Display extends Component {
+  state = {
+    open: false
+  };
+
+  handleOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleDelete = (e, key) => {
+    this.setState({ open: false });
+    // console.log(key);
+    // this.props.deleteItem(key);
+  };
+
   handleChange = (e, key) => {
     const item = this.props.items[key];
-    // take copy of item
     const updateItem = { ...item, [e.target.name]: e.target.value };
-    console.log(updateItem);
     this.props.updateItem(key, updateItem);
   };
 
   renderItems = key => {
     const items = this.props.items[key];
+    console.log(items);
+    const actions = [
+      <FlatButton label="Cancel" primary={true} onClick={this.handleClose} />,
+      <FlatButton label="Discard" primary={true} onClick={this.handleClose} />
+    ];
 
     return (
       <li className="item-wrapper" key={key}>
@@ -66,8 +88,11 @@ class Display extends Component {
               label="Delete Item"
               labelColor={'#ffffff'}
               backgroundColor="#D32F2F"
-              onClick={() => this.props.deleteItem(key)}
+              onClick={this.handleOpen}
             />
+            <Dialog actions={actions} modal={false} open={this.state.open} onRequestClose={this.handleClose}>
+              Discard Item? This action cannot be undone!
+            </Dialog>
           </CardActions>
         </Card>
       </li>
